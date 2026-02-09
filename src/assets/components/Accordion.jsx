5fc989b1af8523fdd16fa51bd1a52d8c850b7dc6
Salvo-Raia <salvo.raia@outlook.it>
2026-02-09 23:24:45 +0100

    Add handleState function to close open Accordion contents

--- src/assets/components/Accordion.jsx
+++ src/assets/components/Accordion.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import AccordionItem from './AccordionItem.jsx';
 import languages from './languages.js';
 
 export default function Accordion () {
+    const [openState, setOpenState] = useState(null);
+    const handleState = (id) => {
+       if (openState === id) {
+        setOpenState(null); 
+       } else {
+        setOpenState(id)
+       }
+    }
     return (
-        <div>
+        <div className='accordion-wrapper'>
             <div className='accordion-btns'>
                 <ul>
                     {languages.map((item => (
-                        <li key={item.id}><button>{item.title}</button></li>
+                        <li key={item.id}><button onClick={() => handleState(item.id)} >{item.title}</button></li>
                     )))}
                 </ul>
             </div>
             {languages.map((item => (
-                <AccordionItem key={item.id} description={item.description} />
+                <AccordionItem key={item.id} title={item.title} description={item.description} isOpen={openState === item.id} />
             )))}
         </div>
     )
--- src/assets/components/AccordionItem.jsx
+++ src/assets/components/AccordionItem.jsx
@@ -1,7 +1,10 @@
-export default function AccordionItem ({description}) {
-    return <div className='accordion-item'>
-                    <div className='accordion-content'>
-                        <p>{description}</p>
-                    </div>
+export default function AccordionItem ({title, description, isOpen}) {
+    return  <div className="accordion-item">
+                <div className="accordion-title">
+                    <h2>{title}</h2>
                 </div>
+                {isOpen && (<div className='accordion-content'>
+                    <p>{description}</p>
+                </div>)}
+            </div>        
 }
