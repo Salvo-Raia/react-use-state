898cf2a960f129fe297dc2133181015c6475d856
Salvo-Raia <salvo.raia@outlook.it>
2026-02-10 00:26:22 +0100

    Change accordion to dynamic card

--- src/assets/components/Accordion.jsx
+++ src/assets/components/Accordion.jsx
@@ -3,26 +3,18 @@ import AccordionItem from './AccordionItem.jsx';
 import languages from './languages.js';
 
 export default function Accordion () {
-    const [openState, setOpenState] = useState(null);
-    const handleState = (id) => {
-       if (openState === id) {
-        setOpenState(null); 
-       } else {
-        setOpenState(id)
-       }
-    }
+    const [selectedIndex, setselectedIndex] = useState(0);
+
     return (
         <div className='accordion-wrapper'>
             <div className='accordion-btns'>
                 <ul>
-                    {languages.map((item => (
-                        <li key={item.id}><button onClick={() => handleState(item.id)} >{item.title}</button></li>
-                    )))}
+                    {languages.map((item, index) => (
+                        <li key={index}><button onClick={() => setselectedIndex(index)}>{item.title}</button></li>
+                    ))}
                 </ul>
             </div>
-            {languages.map((item => (
-                <AccordionItem key={item.id} title={item.title} description={item.description} isOpen={openState === item.id} />
-            )))}
+                <AccordionItem title={languages[selectedIndex].title} description={languages[selectedIndex].description} />
         </div>
     )
 }
--- src/assets/components/AccordionItem.jsx
+++ src/assets/components/AccordionItem.jsx
@@ -1,10 +1,10 @@
-export default function AccordionItem ({title, description, isOpen}) {
+export default function AccordionItem ({title, description}) {
     return  <div className="accordion-item">
                 <div className="accordion-title">
                     <h2>{title}</h2>
                 </div>
-                {isOpen && (<div className='accordion-content'>
+                <div className='accordion-content'>
                     <p>{description}</p>
-                </div>)}
+                </div>
             </div>        
 }
